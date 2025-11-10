e50e1c37ed70062d7ff1597c7fce5724480aa75f
jojozagjos <josephgslade@gmail.com>
2025-11-10 13:53:04 -0700

    build fix again

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,19 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: { port: 3000 },
   build: {
-    chunkSizeWarningLimit: 1200,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('three')) return 'vendor_three'
-            if (id.includes('react')) return 'vendor_react'
-            if (id.includes('socket.io-client')) return 'vendor_socket'
-            return 'vendor'
-          }
-        }
-      }
-    }
+    chunkSizeWarningLimit: 1200
   }
 })
 
